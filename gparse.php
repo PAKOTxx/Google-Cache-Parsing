3712d72c2e87c39f008c6ff76eee8638f3f3711e
PAKOTxx <mainnmaill@gmail.com>
2021-03-16 16:01:43 +0200

    new time out

--- gparse.php
+++ gparse.php
@@ -29,7 +29,7 @@ function gparse($host, $file)
                         }
                     }
                 }
-                sleep(rand(10, 30)); //timeout
+                sleep(rand(10, 20)); //timeout
             }
         }
     }
